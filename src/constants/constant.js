06ae4678c17db4e5c05b4e368245bea798edba1e
parth <parthtrivedi0710@gmail.com>
2023-06-10 12:16:41 +0530

    Updated

---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://cdn.vectorstock.com/i/1000x1000/74/45/recipe-word-text-with-green-leaf-logo-icon-design-vector-24667445.webp';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
